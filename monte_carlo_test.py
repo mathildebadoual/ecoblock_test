9e5499a8fefb0b9b8f307268541a4cd31adcad10
Mathilde Badoual <mathilde.badoual@gmail.com>
2018-01-24 09:12:18 -0800

    add scrapper

--- monte_carlo_test.py
+++ monte_carlo_test.py
@@ -1,2 +1,14 @@
 import numpy as np
+import get_data as get_data
 
+
+# irradiance in 1 kW/m**2 = 1 sun
+
+def pv_generation(irradiance):
+    open_circuit_voltage = 0.612
+    temperature = 25
+    coeff = 0.0257
+    return coeff * (open_circuit_voltage + np.log(irradiance))
+
+
+def
