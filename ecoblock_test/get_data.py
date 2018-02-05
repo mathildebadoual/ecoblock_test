47b855b19ba87eee4391ae83c8077d65ec7edc69
Mathilde Badoual <mathilde.badoual@gmail.com>
2018-02-04 16:10:51 -0800

    add get_data

--- ecoblock_test/get_data.py
+++ ecoblock_test/get_data.py
@@ -5,15 +5,16 @@ cur = conn.cursor()
 
 
 def import_load_demand(start_date, end_date):
-    sql_script = ('SELECT  demand FROM results WHERE time >= start_date AND time <= end_date')
+    sql_script = ('SELECT  demand FROM results WHERE timestamp>= start_date AND timestamp <= end_date')
     sql = cur.execute(sql_script)
     return cur.fetchall()
 
 def import_ev_demand(strat_date, end_date):
-    sql_script = ('SELECT ev_demand FROM results WHERE time >=start_date ADN tine <= end_date')
+    sql_script = ('SELECT ev_demand FROM results WHERE timestamp >=start_date ADN timestamp <= end_date')
     sql = cur.execulte(sql_script)
 
-
-print(import_load_demand(
+start_date = 1493600340
+end_date = 1493683140
+print(import_load_demand(start_date, end_date))
 conn.close()
 
