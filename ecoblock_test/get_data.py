c5bfb6f94c087fc93b3be430df8a22a5d8ca0862
Mathilde Badoual <mathilde.badoual@gmail.com>
2018-02-04 22:46:49 -0800

    update

--- ecoblock_test/get_data.py
+++ ecoblock_test/get_data.py
@@ -1,20 +1,44 @@
 import sqlite3
+import numpy as np
+import pandas as pd
 
-conn = sqlite3.connect('/Users/mathildebadoual/code/ecoblock_test/data/load_demand/astrodek.sqlite')
-cur = conn.cursor()
 
+DATA_PATH = '/Users/mathildebadoual/code/ecoblock_test/data/load_demand/astrodek.sqlite'
 
 def import_load_demand(start_date, end_date):
-    sql_script = ('SELECT  demand FROM results WHERE timestamp>= start_date AND timestamp <= end_date')
-    sql = cur.execute(sql_script)
-    return cur.fetchall()
+    conn = sqlite3.connect(DATA_PATH)
+    cur = conn.cursor()
+    sql_script = ('SELECT demand FROM results WHERE timestamp >= ? AND timestamp <= ? ')
+    cur.execute(sql_script, (start_date, end_date))
+    load_demand = cur.fetchall()
+    conn.close()
+    return np.array(load_demand)
 
-def import_ev_demand(strat_date, end_date):
-    sql_script = ('SELECT ev_demand FROM results WHERE timestamp >=start_date ADN timestamp <= end_date')
-    sql = cur.execulte(sql_script)
+def import_ev_demand(start_date, end_date):
+    conn = sqlite3.connect(DATA_PATH)
+    cur = conn.cursor()
+    sql_script = ('SELECT ev_demand FROM results WHERE timestamp >= ? AND timestamp <= ? ')
+    cur.execute(sql_script, (start_date, end_date))
+    ev_demand = cur.fetchall()
+    conn.close()
+    return np.array(ev_demand)
 
-start_date = 1493600340
-end_date = 1493683140
-print(import_load_demand(start_date, end_date))
-conn.close()
+def import_pv_generation(start_date, end_date):
+    data = pd.read_csv('data/ecoblock_irr.csv')
+    irradiance = data['irradiance'][(data['unix_epoch'] >= start_date) && (data['unix_epoch'] >= end_date)].as_matrix()
+
+
+def import_prices_to_buy(start_date, end_date):
+    data = pd.read_csv('data/prices_a10.csv')
+    prices_to_buy_day = data['prices_to_buy_summer'].as_matrix()
+    simulation_horizon = (start_date - end_date) / 3600
+    prices_to_buy = prices_to_buy_day
+    for _ in range(simulation_horizon / 24 - 1):
+        prices_to_buy = np.concatenate(prices_to_buy, prices_to_buy_day)
+    return prices_to_buy
+
+
+def import_prices_to_sell(start_date, end_date):
+    simulation_horizon = (start_date - end_date) / 3600
+    return np.array([0.07] * (simulation_horizon / 24))
 
--- ecoblock_test/monte_carlo_test.py
+++ ecoblock_test/monte_carlo_test.py
@@ -1,7 +1,9 @@
 import numpy as np
 import get_data as get_data
 import random
-
+import ecoblock_test.get_data as get_data
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdate
 
 # irradiance in 1 kW/m**2 = 1 sun
 
@@ -12,68 +14,107 @@ def pv_generation(irradiance):
     return coeff * (open_circuit_voltage + np.log(irradiance))
 
 
-
-class IndividualHome:
-    def __init__(self, start_date, end_date):
-        self.storage_battery_soc = []
-        self.ev_battery_soc = []
-        self.start_date = start_date
-        self.end_date = end_date
-        self.time
-
-
-    def battery_charged(self, energy_stored):
-        self.storage_battery_soc += energy_stored
-
-    def ev_charging(self, energy_charging):
-        if self.car_is_here:
-            self.ev_battery_soc += energy_charging
-
-    def car_is_here(self):
-        #to improve with the distribution from data
-        return random.randint(0, 1)
-
-    def set_solar_generatioin(self):
-        self.solar_generation = get_data.load_pv_irradiance(self.start_date, self.end_date)[0]
-        # create dictionnary for the date->key and solar_gen->values
-
-    def get_solar_generation(self, date):
-        return self.solar_generation[date]
-
-    def set_consummer_load(self):
-        self.consummer_load = get_data.load_pv_irradiance(self.start_date, self.end_date)[0]
-        # create dictionnary for the date->key and solar_gen->values
-
-    def get_consummer_load(self, date):
-        return self.consummer_load[date]
-
-
-
-
-
-
-
-
 class System:
     def __init__(self, start_time, end_time):
-        # add function to have a finite number of interval of size decided (here 15 min)
         self.start_time = start_time
         self.end_time = end_time
-        self.solar_consumption = []
-        self.
-
-    def generate_houses(self):
-        pass
+        self.time_step = 3600
+        slef.simulation_horizon = (self.end_time - self.start_time) / self.time_step
+        self.load_from_grid = [0]*self.simulation_horizon
+        self.load_to_grid = [0]*self.simulation_horizon
+        self.ref_price_to_buy = 10
+        self.ref_price_to_sell = 10
+        self.flywheel = FlyWheel(self.simulation_horizon, self)
 
     def run_simulation(self):
-        pass
+        for i in range(self.simulation_horizon):
+            unchanged_consumption = self.load_demand[i] + self.ev_demand[i] - selfpv_generation[i]
+            price_to_sell = self.prices_to_sell[i]
+            price_to_buy = self.prices_to_buy[i]
+            if unchanged_consumption < 0:
+                if self.flywheel.is_empty() or price_to_buy <= self.ref_price_to_buy:
+                    self.load_from_grid[i] += unchanged_consumption
+                else :
+                    self.flywheel.discharge(unchanged_consumption, i)
+            if unchanged_consumption > 0:
+                if self.flywheel.is_full() or price_to_sell >= self.ref_price_to_sell:
+                    self.load_to_grid[i] += unchanged_consumption
+                else:
+                    self.flywheel.charge(unchanged_consumption, i)
 
-    def run_step(self):
-        pass
-
-    def next_step(self):
-        pass
+    def load_data(self):
+        self.load_demand = get_data.import_load_demand(self.start_date, self.end_date)
+        self.ev_demand = get_data.import_ev_demand(self.start_date, self.end_date)
+        self.pv_generation = get_data.import_pv_generation(self.start_date, self.end_date)
+        self.prices_to_sell = get_data.import_prices_to_sell(self.start_date, self.end_date)
+        self.prices_to_buy = get_data.import_prices_to_buy(self.start_date, self.end_date)
 
     def plot_results(self):
-        pass
+        dates = [self.start_date + 3600*i for i in range(self.simulation_horizon)]
+        dates = mdate.epo2num(dates)
+        date_fmt = '%d-%m-%y %H:%M:%S'
+        date_formatter = mdate.DateFormatter(date_fmt)
+
+        to_plot = [(self.load_from_grid, 'load from grid'), (0.07*self.load_to_grid, 'load to grid'), (self.flywheels.soc_record, 'flywheel load'),
+                (self.load_demand, 'load demand'), (self.ev_demand, 'ev demand'), (self.pv_generation, 'pv generation')]
+        plt.figure(figsize=(10, 5))
+
+        for i in range(len(to_plot)):
+            fig, ax = plt.subplot(61i)
+            ax.plot_date(dates, to_plot[i][0])
+            ax.xaxis.set_major_formatter(date_formater)
+            plt.title(to_plot[i][0])
+            plt.xlabel('time')
+        plt.show()
+
+
+class FlyWheel:
+    def __init__(self, simulation_horizon, system):
+        self.soc_record = [0]*simulation_horizon
+        self.capacity = 480
+        self.charging_rate_max_in_hour = 120
+        self.discharging_rate_max_in_hour = - 120
+        self.system = system
+
+    def is_full(self):
+        return sum(self.soc_record) == self.capacity
+
+    def is_empty(self):
+        return sum(self.soc_record) == 0
+
+    def charge(self, energy, index):
+        if not self.is_not_full():
+            if (self.capacity - sum(self.soc_record)) >= energy:
+                if self.charging_rate_max_in_hour >= energy:
+                    self.soc_record[index] += energy
+                else:
+                    self.soc_record[index] += self.charging_rate_max_in_hour
+                    self.system.load_to_grid[index] += energy - self.charging_rate_max_in_hour
+            else:
+                energy_enabled = self.capacity - sum(self.soc_record)
+                if self.charging_rate_max_in_hour >= energy_enabled:
+                    self.soc_record[index] += energy_enabled
+                else:
+                    self.soc_record[index] += self.charging_rate_max_in_hour
+                    self.system.load_to_grid[index] += energy_enabled - self.charging_rate_max_in_hour
+                self.system.load_to_grid[index] += energy - energy_enabled
+
+    def discharge(self, energy, index): #energy needs to be > 0
+        if not self.is_not_empty():
+            if sum(self.soc_record) >= energy:
+                if self.discharging_rate_max_in_hour >= energy:
+                    self.soc_record[index] += - energy
+                else:
+                    self.soc_record[index] += self.discharging_rate_max_in_hour
+                    self.system.load_from_grid[index] += energy - self.discharging_rate_max_in_hour
+            else:
+                energy_enabled = sum(self.soc_record)
+                if self.discharging_rate_max_in_hour >= energy_enabled:
+                    self.soc_record[index] += - energy_enabled
+                else:
+                    self.soc_record[index] += self.discharging_rate_max_in_hour
+                    self.system.load_from_grid[index] += - energy_enabled + self.discharging_rate_max_in_hour
+                self.system.load_from_grid[index] += - energy + energy_enabled
+
+
 
